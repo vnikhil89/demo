97b674ae3f24011856ef8a7865497370b2040529
Nikhil Verma <nikhil.nv.verma@oracle.com>
2023-08-21 10:18:06 +0000

    Domain Join Powershell script

---
+++ domain.ps1
@@ -0,0 +1,199 @@
+$name = hostname
+$User = "XXXXXXX\administrator"
+$PWord = ConvertTo-SecureString -String "XXXXXXX" -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+Add-Computer -ComputerName $name -DomainName "horizonvdi.local" -Credential $cred
+function test {
+    param (
+      [String]$Name
+    )
+    $data = foreach($line in Get-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf”)
+  {
+    if($line -like "*$Name*" )
+    {
+    }
+    else
+    {
+     $line
+    }
+  }
+  $data |Set-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf” -Force
+  }
+  test -Name metadata
+  test -Name plugins$name = hostname
+$User = "XXXXXXX\administrator"
+$PWord = ConvertTo-SecureString -String "XXXXXXX" -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+Add-Computer -ComputerName $name -DomainName "horizonvdi.local" -Credential $cred
+function test {
+    param (
+      [String]$Name
+    )
+    $data = foreach($line in Get-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf”)
+  {
+    if($line -like "*$Name*" )
+    {
+    }
+    else
+    {
+     $line
+    }
+  }
+  $data |Set-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf” -Force
+  }
+  test -Name metadata
+  test -Name plugins$name = hostname
+$User = "XXXXXXX\administrator"
+$PWord = ConvertTo-SecureString -String "XXXXXXX" -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+Add-Computer -ComputerName $name -DomainName "horizonvdi.local" -Credential $cred
+function test {
+    param (
+      [String]$Name
+    )
+    $data = foreach($line in Get-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf”)
+  {
+    if($line -like "*$Name*" )
+    {
+    }
+    else
+    {
+     $line
+    }
+  }
+  $data |Set-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf” -Force
+  }
+  test -Name metadata
+  test -Name plugins$name = hostname
+$User = "XXXXXXX\administrator"
+$PWord = ConvertTo-SecureString -String "XXXXXXX" -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+Add-Computer -ComputerName $name -DomainName "horizonvdi.local" -Credential $cred
+function test {
+    param (
+      [String]$Name
+    )
+    $data = foreach($line in Get-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf”)
+  {
+    if($line -like "*$Name*" )
+    {
+    }
+    else
+    {
+     $line
+    }
+  }
+  $data |Set-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf” -Force
+  }
+  test -Name metadata
+  test -Name plugins$name = hostname
+$User = "XXXXXXX\administrator"
+$PWord = ConvertTo-SecureString -String "XXXXXXX" -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+Add-Computer -ComputerName $name -DomainName "horizonvdi.local" -Credential $cred
+function test {
+    param (
+      [String]$Name
+    )
+    $data = foreach($line in Get-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf”)
+  {
+    if($line -like "*$Name*" )
+    {
+    }
+    else
+    {
+     $line
+    }
+  }
+  $data |Set-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf” -Force
+  }
+  test -Name metadata
+  test -Name plugins$name = hostname
+$User = "XXXXXXX\administrator"
+$PWord = ConvertTo-SecureString -String "XXXXXXX" -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+Add-Computer -ComputerName $name -DomainName "horizonvdi.local" -Credential $cred
+function test {
+    param (
+      [String]$Name
+    )
+    $data = foreach($line in Get-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf”)
+  {
+    if($line -like "*$Name*" )
+    {
+    }
+    else
+    {
+     $line
+    }
+  }
+  $data |Set-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf” -Force
+  }
+  test -Name metadata
+  test -Name plugins$name = hostname
+$User = "XXXXXXX\administrator"
+$PWord = ConvertTo-SecureString -String "XXXXXXX" -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+Add-Computer -ComputerName $name -DomainName "horizonvdi.local" -Credential $cred
+function test {
+    param (
+      [String]$Name
+    )
+    $data = foreach($line in Get-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf”)
+  {
+    if($line -like "*$Name*" )
+    {
+    }
+    else
+    {
+     $line
+    }
+  }
+  $data |Set-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf” -Force
+  }
+  test -Name metadata
+  test -Name plugins$name = hostname
+$User = "XXXXXXX\administrator"
+$PWord = ConvertTo-SecureString -String "XXXXXXX" -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+Add-Computer -ComputerName $name -DomainName "horizonvdi.local" -Credential $cred
+function test {
+    param (
+      [String]$Name
+    )
+    $data = foreach($line in Get-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf”)
+  {
+    if($line -like "*$Name*" )
+    {
+    }
+    else
+    {
+     $line
+    }
+  }
+  $data |Set-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf” -Force
+  }
+  test -Name metadata
+  test -Name plugins$name = hostname
+$User = "XXXXXXX\administrator"
+$PWord = ConvertTo-SecureString -String "XXXXXXX" -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+Add-Computer -ComputerName $name -DomainName "horizonvdi.local" -Credential $cred
+function test {
+    param (
+      [String]$Name
+    )
+    $data = foreach($line in Get-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf”)
+  {
+    if($line -like "*$Name*" )
+    {
+    }
+    else
+    {
+     $line
+    }
+  }
+  $data |Set-Content “/Users/niks/Documents/cloudbase/cloudbase-init.conf” -Force
+  }
+  test -Name metadata
+  test -Name plugins
